a66bf33c3df3a815c2c00afbf6b0518eadbb545a
rodoxbpl <rodox.br@gmail.com>
2023-06-28 09:55:30 -0300

    ajuste tratamentos de erros

--- assets/js/consulta-cep.js
+++ assets/js/consulta-cep.js
@@ -1,48 +1,59 @@
 /****** Preenchimento endereço automático através da api viacep ******/
 
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
+  var formErro = document.getElementById('erro-form');
+  formErro.classList.remove('formulario__erro');
 
-    var consulta = `https://viacep.com.br/ws/${cep.value}/json/`;
+  var mensagemErro = document.getElementById('erro-texto');
+  mensagemErro.innerHTML = "";
+
+  var imgErro = document.getElementById('erro-img');
+  imgErro.classList.remove('erro__imagem');
   
+  try {
+    var consulta = `https://viacep.com.br/ws/${cep}/json/`;
+
     var consultaCep = await fetch(consulta);
     var consultaCepConvertida = await consultaCep.json();
-  
-    atualizaCampoEndereco(consultaCepConvertida);
-  }
-  
-  function atualizaCampoEndereco(consultaEndereco) {
-    
-    logradouro.value = consultaEndereco.logradouro;
-  
-    const bairro = document.getElementById('bairro');
-    bairro.value = consultaEndereco.bairro;
-  
-    const cidade = document.getElementById('cidade');
-    cidade.value = consultaEndereco.localidade;
-  
-    const uf = document.getElementById('estado');
-    uf.value = consultaEndereco.uf;
-  }
-  
-  
-  /** chamada callback após interação com campo de cep **/
-  const logradouro = document.getElementById('endereco');
-  const cep = document.getElementById('cep');
-  
-  cep.onkeydown = function (evento) {
-    if (evento.code === "Tab" || evento.code === "Enter") {
-      buscaEndereco();
-    }
-  }
-  
-  logradouro.addEventListener("click", () => {
-    buscaEndereco();
-  });
 
-  const btnFormulario = document.getElementById('enviar');
-  btnFormulario.addEventListener("submit", () => {
-    window.location.href = "cadastro-finalizado.html";
-  })
+    if (consultaCepConvertida.erro){
+      throw Error('CEP não existente!');
+    }
+
+    atualizaCampoEndereco(consultaCepConvertida);
+  } catch(erro) {
+    formErro.classList.add('formulario__erro');
+    mensagemErro.innerHTML = '<p class="erro__texto">CEP inválido. Tente novamente!</p>'
+    imgErro.classList.add('erro__imagem');
+  }
+}
+
+function atualizaCampoEndereco(consultaEndereco) {
+
+  const logradouro = document.getElementById('endereco');
+  logradouro.value = consultaEndereco.logradouro;
+
+  const bairro = document.getElementById('bairro');
+  bairro.value = consultaEndereco.bairro;
+
+  const cidade = document.getElementById('cidade');
+  cidade.value = consultaEndereco.localidade;
+
+  const uf = document.getElementById('estado');
+  uf.value = consultaEndereco.uf;
+}
+
+/** chamada callback após interação com campo de cep **/
+
+const cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => {
+  buscaEndereco(cep.value);
+});
+
+const btnFormulario = document.getElementById('enviar');
+btnFormulario.addEventListener("submit", () => {
+  window.location.href = "cadastro-finalizado.html";
+});
 
   // var consultaCep = fetch('https://viacep.com.br/ws/31970170/json/')
 // .then(resposta => resposta.json())
