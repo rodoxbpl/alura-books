54ce8a72f960f0364c9262c05120b308d851bba7
rodoxbpl <rodox.br@gmail.com>
2023-07-12 19:24:40 -0300

    ajuste chamadas de telas

--- assets/js/file.js
+++ assets/js/file.js
@@ -7,19 +7,6 @@ botaoMenu.addEventListener('click', () => {
   botaoMenu.classList.toggle('cabecalho__menu-hamburguer-ativo');
 })
 
-const swiper = new Swiper('.swiper', {
-  spaceBetween: 10,
-  slidesPerView: 3,
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
 const btnPerfil = document.getElementById('perfil');
 btnPerfil.addEventListener("click", () => {
   window.location.href = "cadastro.html";
